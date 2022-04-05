184c395b500e40b33a63e8ba62891221976ec4a4
lonsmin <marcel@lonsmin.cz>
2022-04-05 12:55:13 +0200

    hedar_ver1

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button color="green" />
+    </header>
+</template>
+<script>
+import Button from './subcomponents/Button.vue'
+export default {
+    name: 'Header',
+    props:{
+        title:{
+            type: String,
+            default: 'Posts',
+        }
+    },
+    components:{
+        Button,
+    }
+}
+</script>
+<style scoped>
+    header{
+        display: flex;
+        justify-content: space-between;
+        align-items:  center;
+        margin-bottom: 20px;
+    }
+</style>
---
+++ src/components/subcomponents/Button.vue
@@ -0,0 +1,29 @@
+<template>
+    <button class="btn"
+    :style="{background: color}"
+    @click="loadPost()"
+    >
+    {{text}}
+    </button>
+</template>
+<script>
+
+
+export default {
+    name: 'Button',
+    props:{
+        text:{
+            type:String,
+            default:'Tlačítko'
+        },
+        color:{
+            type:String,            
+        }
+    },
+    methods:{
+        loadPost(){
+            console.log('loading...')
+        }
+    }
+}
+</script>
